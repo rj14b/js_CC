bb34518d813179539fcb9aa728f56da89f64294d
rj14b <114178383+rj14b@users.noreply.github.com>
2023-09-21 14:47:54 +0000

    config done

---
+++ 01_Basics/07_Operations.js
@@ -0,0 +1,21 @@
+//////////////////Operations////////////////////////
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = " Raj"
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log(1+"2"+2);
+
+console.log(true);
+console.log(+true);
+console.log(+"");
