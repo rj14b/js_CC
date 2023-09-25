0696c3e3e41934d87551e5d1b26c1062d574247c
rj14b <114178383+rj14b@users.noreply.github.com>
2023-09-25 15:40:36 +0000

    config done

---
+++ 01_Basics/num_and_maths.js
@@ -0,0 +1,21 @@
+const score =100
+
+const balance = new Number(100)
+console.log(balance);
+
+
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 23.9683
+console.log(otherNumber.toPrecision(3));
+
+
+
+//////////Maths/////////////
+
+console.log(Math.abs(-4));
+console.log(Math.round(5.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
