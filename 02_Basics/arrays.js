6a03c0d6e56dfa192ee1631d204f230088d50154
rj14b <114178383+rj14b@users.noreply.github.com>
2023-09-26 15:19:24 +0000

    config done

---
+++ 02_Basics/arrays.js
@@ -0,0 +1,26 @@
+//Arrays: defined in []
+
+const myArr = [0,1,2,3,4,5]
+console.log(myArr);
+
+const Heroes = ["spider","iron","hulk"]
+console.log(Heroes);
+
+const Arr = new Array(1,2,3,4,5)
+console.log(Arr[2]);
+
+//Arr methods
+
+
+Arr.push(8)
+console.log(Arr);
+Arr.pop(4)
+console.log(Arr);
+Arr.unshift(10)
+console.log(Arr);
+Arr.shift(2)
+console.log(Arr);
+
+
+console.log(Arr.includes(9));
+console.log(Arr.indexOf(4));
