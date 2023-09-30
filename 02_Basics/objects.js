b693113f37dae72640b6acfbc60cba1b65f4ce89
rj14b <114178383+rj14b@users.noreply.github.com>
2023-09-30 10:26:08 +0000

    done

---
+++ 02_Basics/objects.js
@@ -0,0 +1,13 @@
+//Objects literals
+
+//const JsUser = {}         syntax
+
+const JsUser = {
+    name :" Raj ",
+    age : 18,
+    email : "raj.gmail.com",
+    location : "Mumbai"
+
+}
+
+console.log(JsUser["email"]);
